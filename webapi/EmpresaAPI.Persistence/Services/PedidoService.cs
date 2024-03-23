e6fc6b0e06a34430083b303b3189326c1e05c7e5
NielDevSft <85716027+NielDevSft@users.noreply.github.com>
2024-03-23 19:48:54 -0300

    codigo otimizado

--- webapi/EmpresaAPI.Persistence/Services/PedidoService.cs
+++ webapi/EmpresaAPI.Persistence/Services/PedidoService.cs
@@ -1,53 +1,63 @@
 ﻿using EmpresaAPI.Domain.Pedidos;
+using EmpresaAPI.Domain.Pedidos.Itens;
 using EmpresaAPI.Domain.Pedidos.Itens.Repository;
-using EmpresaAPI.Domain.Pedidos.ItensPedidos;
 using EmpresaAPI.Domain.Pedidos.ItensPedidos.Repository;
+using EmpresaAPI.Domain.Pedidos.ItensPedidos;
 using EmpresaAPI.Domain.Pedidos.Repository;
 using EmpresaAPI.Domain.Pedidos.Service;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace EmpresaAPI.Persistence.Services
 {
-    public class PedidoService(IPedidoRepository pedidoRepository,
-        IItemPedidoRepository itemPedidoRepository,
-        IItemRepository itemRepository) : IPedidoService
+    public class PedidoService : IPedidoService
     {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly IItemPedidoRepository _itemPedidoRepository;
+        private readonly IItemRepository _itemRepository;
+
+        public PedidoService(
+            IPedidoRepository pedidoRepository,
+            IItemPedidoRepository itemPedidoRepository,
+            IItemRepository itemRepository)
+        {
+            _pedidoRepository = pedidoRepository;
+            _itemPedidoRepository = itemPedidoRepository;
+            _itemRepository = itemRepository;
+        }
+
         public async Task<Pedido> Create(Pedido pedido)
         {
-
             var itensPedidosTratados = new List<ItemPedido>();
-            foreach (var itemPedido in pedido
-            .ItensPedido)
+            foreach (var itemPedido in pedido.ItensPedido)
             {
-                var item = await itemRepository.GetById(itemPedido.Item!.Uuid);
+                var item = await _itemRepository.GetById(itemPedido.Item!.Uuid);
                 itemPedido.ItemUuid = itemPedido.Item!.Uuid;
                 itemPedido.Item = null;
                 itensPedidosTratados.Add(itemPedido);
             }
             pedido.ItensPedido = itensPedidosTratados;
-            await pedidoRepository.Add(pedido);
-
-            await itemPedidoRepository.SaveChanges();
+            await _pedidoRepository.Add(pedido);
+            await _itemPedidoRepository.SaveChanges();
             return pedido;
         }
 
         public async Task Delete(Guid uuid)
         {
-            var pedido = await pedidoRepository.GetById(uuid);
-            if (pedido! != null!)
+            var pedido = await _pedidoRepository.GetById(uuid);
+            if (pedido != null)
             {
-                await pedidoRepository.Remove(pedido.Uuid);
-                await pedidoRepository.SaveChanges();
-
+                await _pedidoRepository.Remove(pedido.Uuid);
+                await _pedidoRepository.SaveChanges();
             }
         }
 
         public async Task<List<Pedido>> GetAll()
         {
             var pedidoDict = new Dictionary<Guid, Pedido>();
-
-
-            var itemPedidoList = await itemPedidoRepository.FindAllWhere(i => !i.Pedido!.Removed, "Pedido", "Item");
+            var itemPedidoList = await _itemPedidoRepository.FindAllWhere(i => !i.Pedido!.Removed, "Pedido", "Item");
 
             foreach (var itemPedido in itemPedidoList)
             {
@@ -57,42 +67,29 @@ namespace EmpresaAPI.Persistence.Services
                     pedido.ItensPedido = new List<ItemPedido>();
                     pedidoDict.Add(pedido.Uuid, pedido);
                 }
-
                 pedido.ItensPedido.Add(itemPedido);
             }
-
-
             return pedidoDict.Values.ToList();
         }
 
         public async Task<Pedido> GetById(Guid uuid)
         {
-            Pedido pedidoFound;
-
-            pedidoFound = await pedidoRepository
-                .GetById(uuid);
-
-            if (pedidoFound! == null!)
+            var pedidoFound = await _pedidoRepository.GetById(uuid);
+            if (pedidoFound == null)
             {
-                throw new Exception("Item não encontrado");
+                throw new Exception("Pedido não encontrado");
             }
-
             return pedidoFound;
         }
 
         public async Task<Pedido> Update(Guid uuid, Pedido pedido)
         {
-            var pedidoFoundTk = pedidoRepository.GetById(uuid, "ItensPedido");
-            var itensPedidoFoundTk = itemPedidoRepository.FindAllWhere(ip => ip.PedidoUuid == pedido.Uuid);
-            Pedido? pedidoFound;
-            List<ItemPedido> itensPedidoFound = new List<ItemPedido>();
-            await Task.WhenAll(itensPedidoFoundTk, pedidoFoundTk);
-
-            pedidoFound = pedidoFoundTk.Result;
-            itensPedidoFound = itensPedidoFoundTk.Result;
+            var pedidoFound = await _pedidoRepository.GetById(uuid, "ItensPedido");
+            var itensPedidoFound = await _itemPedidoRepository.FindAllWhere(ip => ip.PedidoUuid == pedido.Uuid);
 
             itensPedidoFound = UpdateItemPedidoList(pedido, itensPedidoFound);
-            if (pedidoFound is null)
+
+            if (pedidoFound == null)
             {
                 throw new Exception("Pedido não encontrado");
             }
@@ -101,12 +98,12 @@ namespace EmpresaAPI.Persistence.Services
                 pedidoFound.ProfissionalResponsavel = pedido.ProfissionalResponsavel;
                 pedidoFound.ValorTotal = pedido.ValorTotal;
                 pedidoFound.ItensPedido = itensPedidoFound;
-                await pedidoRepository.Update(pedidoFound);
-                await pedidoRepository.SaveChanges();
-                await itemPedidoRepository.SaveChanges();
 
+                await _pedidoRepository.Update(pedidoFound);
+                await _pedidoRepository.SaveChanges();
+                await _itemPedidoRepository.SaveChanges();
             }
-            return pedidoFound!;
+            return pedidoFound;
         }
 
         private List<ItemPedido> UpdateItemPedidoList(Pedido pedido, List<ItemPedido> itensPedidoFound)
@@ -114,27 +111,22 @@ namespace EmpresaAPI.Persistence.Services
             if (itensPedidoFound.Count > pedido.ItensPedido.Count)
             {
                 var itensARemover = itensPedidoFound.ToList();
-                itensARemover.RemoveAll(ip =>
-                   pedido.ItensPedido.FirstOrDefault(ipf => ipf.QtdItem == ip.QtdItem
-                   && ipf.ItemUuid == ip.ItemUuid) is null);
-                itensARemover.ForEach(ip =>
+                itensARemover.RemoveAll(ip => pedido.ItensPedido.FirstOrDefault(ipf => ipf.QtdItem == ip.QtdItem && ipf.ItemUuid == ip.ItemUuid) == null);
+
+                foreach (var ip in itensARemover)
                 {
-                    itemPedidoRepository.Remove(ip.Uuid);
-                });
-                itensPedidoFound = itensPedidoFound.FindAll(ipf => pedido
-                    .ItensPedido
-                    .FirstOrDefault(ip => ipf.ItemUuid == ip.ItemUuid && ip.QtdItem == ip.QtdItem) is not null);
+                    _itemPedidoRepository.Remove(ip.Uuid);
+                }
+
+                itensPedidoFound = itensPedidoFound.FindAll(ipf => pedido.ItensPedido.FirstOrDefault(ip => ipf.ItemUuid == ip.ItemUuid && ip.QtdItem == ip.QtdItem) != null);
             }
             else
             {
                 var itensExcedentes = pedido.ItensPedido.ToList();
-                var itens = itensExcedentes.RemoveAll(ip =>
-                    itensPedidoFound.FirstOrDefault(ipf => ipf.QtdItem == ip.QtdItem
-                    && ipf.ItemUuid == ip.ItemUuid) is not null);
+                itensExcedentes.RemoveAll(ip => itensPedidoFound.FirstOrDefault(ipf => ipf.QtdItem == ip.QtdItem && ipf.ItemUuid == ip.ItemUuid) != null);
 
                 itensPedidoFound.AddRange(itensExcedentes);
             }
-
             return itensPedidoFound;
         }
     }
